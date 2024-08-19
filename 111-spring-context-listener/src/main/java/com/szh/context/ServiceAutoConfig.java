aef792e95195678c848d91f6c4280bdd1c58cd6e
沈泽辉 <shenzehui@wondersgroup.com>
2024-08-19 17:02:21 +0800

    perf: 使用spi机制解决启动类无法扫描到其他模块配置类的问题

---
+++ 111-spring-context-listener/src/main/java/com/szh/context/ServiceAutoConfig.java
@@ -0,0 +1,17 @@
+package com.szh.context;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Title:
+ * Description:
+ * Company: wondersgroup.com
+ *
+ * @author 沈泽辉
+ * @version 1.0
+ */
+@Configuration
+@ComponentScan("com.szh.context")
+public class ServiceAutoConfig {
+}
