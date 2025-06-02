866fb05f07735884a1235defa77a5b25ba433df4
沈泽辉 <shenzehui@wondersgroup.com>
2025-06-03 00:30:48 +0800

    perf/ 异位词 快乐数 两个数组的交集 四数相加

---
+++ hui-algorithm/src/main/java/com/szh/algorithm/FourSumCount.java
@@ -0,0 +1,45 @@
+package com.szh.algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FourSumCount {
+
+    /**
+     * 454. 四数相加 II
+     * 给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l) 能满足：
+     * 0 <= i, j, k, l < n
+     * nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+     *
+     * @param nums1
+     * @param nums2
+     */
+    public int fourSumCount(int[] nums1, int[] nums2, int[]  nums3, int[] nums4) {
+        // key 是要前两个数组要查询的target，value是出现的次数
+        Map<Integer, Integer> record = new HashMap<>();
+
+        int count = 0;
+
+        for (int value : nums1) {
+            for (int k : nums2) {
+                int sum = value + k;
+                if (record.containsKey(sum)) {
+                    record.put(sum, record.get(sum) + 1);
+                } else {
+                    record.put(sum, 1);
+                }
+            }
+        }
+
+        for (int k : nums3) {
+            for (int i : nums4) {
+                int sum = k + i;
+                int target = -sum;
+                if (record.containsKey(target)) {
+                    count += record.get(target);
+                }
+            }
+        }
+        return count;
+    }
+}
---
+++ hui-algorithm/src/main/java/com/szh/algorithm/HappyNumber.java
@@ -0,0 +1,35 @@
+package com.szh.algorithm;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber {
+    /**
+     * 快乐数
+     *
+     * @param n
+     * @return
+     */
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        int temp = n;
+        while (temp != 1 && !set.contains(temp)) {
+            set.add(temp);
+            temp = getNumber(temp);
+        }
+        // temp 要么等于1，要么就是循环的数
+        return temp == 1;
+    }
+
+
+    public int getNumber(int n) {
+        int res = 0;
+
+        while (n != 0) {
+            int temp = n % 10;
+            res += temp * temp;
+            n = n / 10;
+        }
+        return res;
+    }
+}
