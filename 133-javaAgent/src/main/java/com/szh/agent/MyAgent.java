c896b3aea5748a028c74da1e3fbf34f06a610533
shenzehui <3032388097@qq.com>
2025-08-27 16:37:09 +0800

    feat: javaAgent demo

---
+++ 133-javaAgent/src/main/java/com/szh/agent/MyAgent.java
@@ -0,0 +1,24 @@
+package com.szh.agent;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * Title:
+ * Description:
+ * Company: wondersgroup.com
+ *
+ * @author 沈泽辉
+ * @version 1.0
+ */
+
+public class MyAgent {
+
+    //JVM 首先尝试在代理类上调用以下方法
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.out.println("嗨！JavaAgent " + agentArgs);
+    }
+
+    //如果代理类没有实现上面的方法，那么 JVM 将尝试调用该方法
+    public static void premain(String agentArgs) {
+    }
+}
