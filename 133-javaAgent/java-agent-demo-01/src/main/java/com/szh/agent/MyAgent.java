0502118c064e54f6a085dbe01c6403f5da91e69b
shenzehui <3032388097@qq.com>
2025-08-29 10:14:50 +0800

    refactor: 新增javaAgent知识点测试示例

--- 133-javaAgent/java-agent-demo-01/src/main/java/com/szh/agent/MyAgent.java
+++ 133-javaAgent/java-agent-demo-01/src/main/java/com/szh/agent/MyAgent.java
@@ -2,23 +2,15 @@ package com.szh.agent;
 
 import java.lang.instrument.Instrumentation;
 
-/**
- * Title:
- * Description:
- * Company: wondersgroup.com
- *
- * @author 沈泽辉
- * @version 1.0
- */
-
 public class MyAgent {
 
     //JVM 首先尝试在代理类上调用以下方法
     public static void premain(String agentArgs, Instrumentation inst) {
-        System.out.println("嗨！JavaAgent " + agentArgs);
+        System.out.println("this is my agent：" + agentArgs);
     }
 
     //如果代理类没有实现上面的方法，那么 JVM 将尝试调用该方法
     public static void premain(String agentArgs) {
     }
+
 }
